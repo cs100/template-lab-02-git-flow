364e893a39b9baa6ec9574ff44629235d556b2f5
RobertNavarro <robertn2562@gmail.com>
2020-01-20 12:49:51 -0800

    Moved a bracket so I can attempt to push again

--- c-count.h
+++ c-count.h
@@ -4,7 +4,8 @@ unsigned int count(const std::string& str) {
     unsigned int counter = 0;
     bool on_space = true;
     
-    for(int i = 0; i < str.size(); i++) {
+    for(int i = 0; i < str.size(); i++)
+ {
         if (std::isspace(str[i]))
             on_space = true;
         else if (on_space) {
