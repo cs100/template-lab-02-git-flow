e85c4deaa3279cd7c49c00b0e6cea01fdb1918a5
rgiron1 <rgiro004@hammer.cs.ucr.edu>
2020-01-21 10:10:05 -0800

    Fixed #2

--- c-count.h
+++ c-count.h
@@ -1,4 +1,5 @@
 #include <iostream>
 
-// count function should go here
-
+int count(const std::string& phrase) {
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,11 +1,11 @@
 #include "c-echo.h"
-
+#include "c-count.h"
 #include "gtest/gtest.h"
 
 #include "string"
 
 TEST(EchoTest, HelloWorld) {
-    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
+    char* test_val[3]; test_val[0] = "./c-echo-count"; test_val[1] = "hello"; test_val[2] = "world";
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
@@ -27,6 +27,21 @@ TEST(EchoTest, ProgrammingChar) {
 TEST(EchoTest, SpecialChar) {
 	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "@";
 	EXPECT_EQ("@", echo(2,test_val));
+}
+
+TEST(CountTest, EmptyString) {
+	std::string test_str = "";
+	EXPECT_EQ(0,count(test_str));
+}
+
+TEST(CountTest, HelloWorld) {
+	std::string test_str = "hello world";
+	EXPECT_EQ(2, count(test_str));
+}
+
+TEST(CountTest, ManySpaces){
+	std::string test_str = "  this   string has      weird   spacing";
+	EXPECT_EQ(5, count(test_str));
 }
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
