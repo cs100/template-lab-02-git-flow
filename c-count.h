dfb7db4bab6b234659453df1cbbd70acfe71e659
Sophie-Si <57075778+Sophie-Si@users.noreply.github.com>
2020-08-02 20:54:28 -0700

    Partner 1 updates c_count.h
    * Updates partner 1's branch only

--- c-count.h
+++ c-count.h
@@ -1,4 +1,17 @@
 #include <iostream>
+//Partner 1 changes 2nd time
+unsigned int count(const std::string& str) {
+    unsigned int counter = 0;
+    bool on_space = true;
+    
+    for(int i = 0; i < str.size(); i++) {
+        if (std::isspace(str[i]))
+            on_space = true;
+        else if (on_space) {
+            counter++;
+            on_space = false;
+        }
+    }
 
-// count function should go here
-
+    return counter;
+}
